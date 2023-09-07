fe8c5b19844a8f911cefefa19ff06fd3780e25d9
jia-io <1398106879@qq.com>
2023-09-07 10:05:46 +0800

    update gin

--- internal/leo.go
+++ internal/leo.go
@@ -1,9 +1,22 @@
-package internal
+package main
 
-import "leo/internal/dal"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
-func init() {
-}
-func Leo() {
-	dal.NewDatabase()
+func main() {
+	// 创建一个新的 Gin 路由引擎
+	r := gin.Default()
+
+	// 定义一个简单的路由，处理 GET 请求
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, Gin!",
+		})
+	})
+
+	// 启动服务器，默认端口为 8080
+	_ = r.Run()
+
 }
